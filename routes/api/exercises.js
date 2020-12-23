6a23463cddaad3a711199bc8e0b096217b745d05
gombarova <gombarova@users.noreply.github.com>
2020-12-23 18:12:01 +0100

    updated exercises.js

--- routes/api/exercises.js
+++ routes/api/exercises.js
@@ -13,9 +13,10 @@ router.post('/', async (req, res) => {
 
     await saveExercise(constants, predicates, functions, propositions);
 
-    res.json(req.body);
+    res.status(200).json(req.body);
   } catch (err) {
     console.error(err.stack);
+    res.status(500).json({ message: "Something went wrong" });
   }
 });
 
