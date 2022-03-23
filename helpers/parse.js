82b4ef9d64f11b03155d3b62ee8beb48321e78d0
nikolakulikova <kulikova.n12345@gmail.com>
2022-03-23 12:42:22 +0100

    rewrited getting constants, predicates and functions from vampire output

--- helpers/parse.js
+++ helpers/parse.js
@@ -1,160 +1,212 @@
 const { parseTff } = require('../../js-fol-parser');
+var chance = require('chance').Chance();
+const {
+    getLanguage
+} = require('./checks');
+const {Variable, Constant, FunctionApplication, PredicateAtom,
+    EqualityAtom, Negation, Conjunction, Disjunction, Implication,
+    Equivalence, ExistentialQuant, UniversalQuant} = require('./formula_classes');
+const constants = require("constants");
 
-function getStructure(structure){
+function getStructure(structure, language, exercise){
     structure = structure.split(".");
-
-    let constants = {};
+    let constants = {}
     let predicates = {};
+    let functions = {};
     let poc = 1;
+
+    getLanguage(exercise).constants.forEach(key => constants[key] = poc);
+    poc++;
+    getLanguage(exercise).predicates.forEach((key, value) => predicates[value] = []);
+    getLanguage(exercise).functions.forEach((key, value) => functions[value] = []);
 
     for(let i = 0; i < structure.length - 1; i++ ){
         if(structure[i] === "\n" ){
             continue;
         }
-        let formula = structure[i] + ".";
-        let parsed_formula = parseTff(formula, factories());
-        if(parsed_formula.type === "type" || (!parsed_formula.name.includes("predicate") && !parsed_formula.name.includes("function")) ){
+        let parsed_formula = parseTff(structure[i] + ".", factories(language.constantsToOriginal, language.variablesToOriginal,
+            language.functionsToOriginal, language.predicatesToOriginal ));
+        if(parsed_formula.type === "type"){
             continue;
         }
-        let name = parsed_formula.name.slice(parsed_formula.name.search("_") + 1, parsed_formula.name.length);
-        let pom;
+
         if(parsed_formula.name.includes("predicate")){
-            pom = parsePredicate(parsed_formula, poc, predicates, constants, name);
+            if(parsed_formula.formula.constructor.name === "Conjunction"){
+                poc = parsePredicate(parsed_formula, poc, predicates, constants).poc;
+            }
         }
-
         if(parsed_formula.name.includes("function")){
-            pom = parseFunction(parsed_formula, poc, predicates, constants, name);
+            let functionAplications ;
+            if(parsed_formula.formula.constructor.name === "Conjunction"){
+                functionAplications = parsed_formula.formula.getAll()
+            }
+            else if (parsed_formula.formula.constructor.name === "FunctionApplication"){
+                functionAplications = [parsed_formula.formula];
+            }
+            poc =  parseFunction(parsed_formula, poc, functions, constants,functionAplications).poc;
         }
-        predicates = pom.predicates;
-        constants = pom.constants;
-        poc = pom.poc;
+        if(parsed_formula.name.includes("definition")){
+            let constant ;
+                if(parsed_formula.formula.constructor.name === "Conjuction") {
+                    constant = parsed_formula.formula.getAll()
+                }
+                if(parsed_formula.formula.constructor.name === "Disjunction") {
+                    constant = parsed_formula.formula.getAll()
+                }
+                if (parsed_formula.formula.constructor.name === "EqualityAtom") {
+                    constant = parsed_formula.formula.getAll()
+                } else {
+                    constant = [parsed_formula.formula];
+                }
+                for (let i = 0; i < constant.length; i += 2) {
+                    poc = parseEqualityAtom(constant[i], constant[i + 1], constants, poc).poc;
+                }
+
+        }
+        if(parsed_formula.name.includes("finite_domain")){
+            if(parsed_formula.formula.constructor.name === "UniversalQuant"){
+                if(parsed_formula.formula.type === "i"){
+                    poc = parseUniQuant(parsed_formula, poc, predicates, constants).poc;
+                }
+            }
+        }
     }
-    return getStringDomainAndPredicates(predicates, constants);
+    for(let [key, value] of Object.entries(functions)){
+        poc = fillFunction(functions, constants, key, poc).poc;
+    }
+    return getStringDomainAndPredicates(predicates, constants, functions, exercise);
+
 }
 
-function  parseFunction(parsed_formula, poc, predicates, constants, predicate){
-    let cons = [];
-    predicates[predicate] = [];
-    if(parsed_formula.formula === "comment" || parsed_formula.formula === "c:"){
-        return;
+function parseFunction(parsed_formula, poc, functions, constants, functionApplications){
+    for(let i = 0; i < functionApplications.length; i++){
+        let args = functionApplications[i].getArgs();
+        let constant = [];
+        for(let j = 0; j < args.length; j++) {
+            if(args[j].constructor.name !== "Array"){
+                args[j] = [args[j]];
+            }
+            for(let m = 0; m < args[j].length; m++ ){
+                poc = parseConstants(args[j][m], constants, poc).poc;
+                if(args[j][m].getOriginalSymbol() === undefined){
+                    constant.push(constants[args[j][m].toVampire()]);
+                }
+                else {
+                    constant.push(constants[args[j][m].getOriginalSymbol()]);
+                }
+            }
+        }
+        functions[functionApplications[i].getSymbol()].push( constant);
     }
-    let formula = parsed_formula.formula;
-
-    if(formula.includes("∧")){
-        formula = formula.split("∧");
-    }
-    else{
-        formula = [formula];
-    }
-    for(let i = 0; i < formula.length; i++){
-        if(formula[i].includes("=")){
-            formula[i] = formula[i].split("=");
-        }
-        let count = (formula[i][0].match(/\(/g) || []).length;
-        if(count !== 1){
-            formula[i][0] = formula[i][0].slice(1, formula[i][0].length);
-        }
-        count = (formula[i][0].match(/\)/g) || []).length;
-        if(count !== 1){
-            formula[i][0] = formula[i][0].slice(0, formula[i][0].length - 1);
-        }
-        if(formula[i][0] === "¬"){
-            continue;
-        }
-        formula[i][0] = formula[i][0].slice(formula[i][0].indexOf("("), formula[i][0].indexOf(")"));
-        let constant = [formula[i][0]];
-        if(formula[i][0].includes(",")){
-            constant = formula[i][0].split(",");
-        }
-        let pom = getConstantsFromPredicate(constant, constants, poc);
-        constants = pom.constants;
-        poc = pom.poc;
-        formula[i][1] = formula[i][1].replaceAll(")", "");
-        formula[i][1] = formula[i][1].replaceAll("(", "");
-        formula[i][1] = formula[i][1].slice(formula[i][1].search(":") + 1, formula[i][1].length);
-        if (!(formula[i][1] in constants)){
-            constants[formula[i][1]] = poc;
-            poc++;
-        }
-
-        let consta = [];
-        for(let m = 0; m < constant.length; m++ ){
-            consta.push(constants[constant[m]]);
-        }
-        consta.push(constants[formula[i][1]]);
-        cons.push(consta);
-    }
-    predicates[predicate] = cons;
-
-    return {constants: constants, poc: poc, predicates: predicates};
+    return{functions: functions, constants: constants, poc: poc};
 }
 
-
-function parsePredicate(parsed_formula, poc, predicates, constants, predicate){
-    let cons = [];
-    predicates[predicate] = [];
-    if(parsed_formula.formula === "comment" || parsed_formula.formula === "c:"){
-        return {constants: constants, poc: poc, predicates: predicates};
+function parsePredicate(parsed_formula, poc, predicates, constants){
+    let res = parsed_formula.formula.getAll();
+    for(let i = 0; i < res.length; i++){
+        let args = res[i].getArgs();
+        let constant = [];
+        for(let j = 0; j < args.length; j++) {
+            poc = parseConstants(args[j], constants, poc).poc;
+            if(args[j].getOriginalSymbol() === undefined){
+                constant.push(constants[args[j].toVampire()]);
+            }
+            else {
+                constant.push(constants[args[j].getOriginalSymbol()]);
+            }
+        }
+        predicates[res[i].getSymbol()].push(constant);
     }
-    let formula = parsed_formula.formula;
-
-    if(formula.includes("∧")){
-        formula = formula.split("∧");
-    }
-    else{
-        formula = [formula];
-    }
-    for(let i = 0; i < formula.length; i++){
-        let count = (formula[i].match(/\(/g) || []).length;
-        if(count > 1){
-            formula[i] = formula[i].slice(1, formula[i].length);
-        }
-        count = (formula[i].match(/\)/g) || []).length;
-        if(count > 1){
-            formula[i] = formula[i].slice(0, formula[i].length - 1);
-        }
-        if(formula[i][0] === "¬"){
-            continue;
-        }
-        formula[i] = formula[i].slice(formula[i].indexOf("("), formula[i].indexOf(")"));
-        let constant = [formula[i]];
-        if(formula[i].includes(",")){
-            constant = formula[i].split(",");
-        }
-        let pom = getConstantsFromPredicate(constant, constants, poc);
-        constants = pom.constants;
-        poc = pom.poc;
-        console.log(constants);
-        let consta = [];
-        for(let m = 0; m < constant.length; m++ ){
-            consta.push(constants[constant[m]]);
-        }
-        cons.push(consta);
-
-    }
-    predicates[predicate] = cons;
-    return {constants: constants, poc: poc, predicates: predicates};
+    return{predicates: predicates, constants: constants, poc: poc};
 }
 
-function getConstantsFromPredicate(constant, constants, poc){
-    for(let j = 0; j < constant.length; j++){
-        constant[j] = constant[j].slice(constant[j].search(":") + 1, constant[j].length);
-        if(constant[j] in constants){
+function parseUniQuant(parsed_formula, poc, predicates, constants){
+    let domain = parsed_formula.formula.subf.getAll();
+    for(let i = 0; i < domain.length; i++){
+       if(domain[i].vampireSymbol === "X"){
+           continue;
+       }
+       poc = parseConstants(domain[i], constants, poc).poc;
+    }
+    return{ constants: constants, poc: poc};
+}
+
+function parseConstants(constant, constants, poc){
+    if(constant.getOriginalSymbol() === ''){
+        return { constants: constants, poc: poc};
+    }
+    for(const key in constants){
+        if(key === constant.getOriginalSymbol() || key === constant.toVampire()){
+            return { constants: constants, poc: poc};
+        }
+    }
+    if(constant.getOriginalSymbol() === undefined){
+        constants[constant.toVampire()] = poc;
+    }
+    else {
+        constants[constant.getOriginalSymbol()] = poc;
+    }
+    poc++;
+    return{ constants: constants, poc: poc};
+}
+
+function parseEqualityAtom(constant1, constant2, constants, poc){
+    if(constants[constant2] !== constants[constant1]){
+        if(constants[constant2] < constants[constant1]){
+            constants[constant1] = constants[constant2];
+        }
+        else{
+            constants[constant2] = constants[constant1];
+        }
+    }
+    return{ constants: constants, poc: poc};
+}
+
+function fillFunction(functions, constants, fun, poc) {
+    let was = []
+    console.log(functions[fun]);
+    for (let value of functions[fun]) {
+        was.push(value[0])
+    }
+    for (let key in constants) {
+        console.log(constants[key]);
+        if (!(was.includes(constants[key]))){
+            was.push(constants[key]);
+            functions[fun].push([constants[key], chance.integer({min: 1, max: poc - 1})]);
+        }
+    }
+    return {constants: constants, poc: poc, functions: functions};
+}
+
+function getStringDomainAndPredicates(predicates, constants, functions, exercise){
+    let d = "D= {";
+    let i = "";
+    let poc = 0;
+    for (let [key, value] of Object.entries(constants)){
+        if(getLanguage(exercise).constants.has(key)) {
+            i += "i(" + key + ") = " + value + "\n";
+        }
+        if( value <= poc){
             continue;
         }
-        constants[constant[j]] = poc;
+        d += value + ", ";
         poc++;
     }
-    return {constants: constants, poc: poc};
+    i += "\n";
+    d = d.slice(0, d.length -2 );
+    d += "}\n";
+
+    i += stringForPredicateAndFunctions(predicates);
+    i += stringForPredicateAndFunctions(functions);
+    return {domain: d, prvky: i};
 }
 
-function getStringDomainAndPredicates(predicates, constants){
-    let d = "D= { ";
+function stringForPredicateAndFunctions(name){
     let p = "";
-    for (const [key, value] of Object.entries(predicates)) {  //ak su v jazyku konstanty pripradit im prvok domeny
+    for (let [key, value] of Object.entries(name)) {
         p += "i(" + key + ") = " + "{";
         if (value[value.length - 1] === undefined) {
-            p += "}\n ";
+            p += "}\n";
             continue;
         }
         for (let j = 0; j < value.length - 1; j++) {
@@ -163,31 +215,26 @@ function getStringDomainAndPredicates(predicates, constants){
             }
             p += "(" + value[j] + ")";
         }
-        p += "(" +  value[value.length - 1] + ")}\n "; //jeden / posledny prvok
+        p += "(" +  value[value.length - 1] + ")}\n";
     }
-    for (const [key, value] of Object.entries(constants)){
-        d += value + " ,";
-    }
-    d = d.slice(0, d.length -1 );
-    d += "}\n ";
-    return {domain: d, predicates: p};
+
+    return p;
 }
 
-function factories(){
-    const application = (sym, args, _) => `${sym}(${args.join(',')})`
+function factories(mapConstant, mapVariable, mapFunction, mapPredicate){
     return{
-        variable: (v, _) => `v:${v}`,
-        constant: (c, _) => `c:${c}`,
-        functionApplication: application,
-        predicateAtom: application,
-        equalityAtom: (lhs, rhs, _) => `${lhs}=${rhs}`,
-        negation: (f, _) => `¬${f}`,
-        conjunction: (lhs, rhs, _) => `(${lhs}∧${rhs})`,
-        disjunction: (lhs, rhs, _) => `(${lhs}∨${rhs})`,
-        implication: (lhs, rhs, _) => `(${lhs}→${rhs})`,
-        equivalence: (lhs, rhs, _) => `(${lhs}↔︎${rhs})`,
-        existentialQuant: (v, f, _) => `∃${v} ${f}`,
-        universalQuant: (v, f, _) => `∀${v} ${f}`,
+        variable: (v, _) => new Variable(mapVariable.get(v) , v),
+        constant: (c, _) => new Constant(mapConstant.get(c), c),
+        functionApplication:  (symbol, args, ee) => new FunctionApplication(mapFunction.get(symbol), symbol, args),
+        predicateAtom: (symbol, args, ee) => new PredicateAtom(mapPredicate.get(symbol), symbol, args),
+        equalityAtom: (lhs, rhs, _) => new EqualityAtom(lhs, rhs),
+        negation: (f, _) => new Negation(f),
+        conjunction: (lhs, rhs, _) => new Conjunction(lhs, rhs),
+        disjunction: (lhs, rhs, _) => new Disjunction(lhs, rhs),
+        implication: (lhs, rhs, _) => new Implication(lhs, rhs),
+        equivalence: (lhs, rhs, _) => new Equivalence(lhs, rhs),
+        existentialQuant: (v, f, _) => new ExistentialQuant(mapVariable.get(v), v, f),
+        universalQuant: (v, type, f, _) => new UniversalQuant(mapVariable.get(v), v, type, f),
     }
 }
 
