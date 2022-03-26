ccb7c1eb53dde89a6bded82c08a2f724b78eb62c
nikolakulikova <kulikova.n12345@gmail.com>
2022-03-26 12:22:33 +0100

    removed console logs and added M=(D, i) to structure output

--- helpers/vampire.js
+++ helpers/vampire.js
@@ -19,6 +19,8 @@ module.exports = async function evalWithVampire(
 ) {
     let eval_status = {
         solutionToFormalization: '',
+        m1: '',
+        m2: '',
         formalizationToSolution: '',
         domainSolutionToFormalization: '',
         iSolutionToFormalization: '',
@@ -32,12 +34,19 @@ module.exports = async function evalWithVampire(
         res.status(200).json(eval_status);
         saveSolutionWithResult(eval_status);
     } else {
+        let notFound = 'Nepodarilo sa nájsť štruktúru, na vaše riešenie sa radšej opýtajte.';
+        let structureSolutionToFormalization = ', v ktorej je vaša formalizácia pravdivá, ale hľadaná správna formalizácia je nepravdivá:'
+        let structureFormalizationToSolution = ', v ktorej je vaša formalizácia nepravdivá, ale hľadaná správna formalizácia je pravdivá:'
         let vampireOutput = await vampireStructure(formalization, solution, timeLimit, language, exercise);
         eval_status.domainFormalizationToSolution = vampireOutput.domain;
-        eval_status.iFormalizationToSolution = vampireOutput.prvky !== undefined ? vampireOutput.prvky : 'Nepodarilo sa nájsť štruktúru, na vaše riešenie sa radšej opýtajte. :(\n' ;
+        eval_status.iFormalizationToSolution = vampireOutput.prvky !== undefined ? vampireOutput.prvky : notFound ;
+        eval_status.m1 =  vampireOutput.m !== '' ? "Štruktúra " + vampireOutput.m + structureFormalizationToSolution
+                            : "Štruktúra" + structureFormalizationToSolution ;
         vampireOutput = await vampireStructure(solution, formalization, timeLimit, language, exercise)
         eval_status.domainSolutionToFormalization = vampireOutput.domain;
-        eval_status.iSolutionToFormalization = vampireOutput.prvky !== undefined ? vampireOutput.prvky : 'Nepodarilo sa nájsť štruktúru, na vaše riešenie sa radšej opýtajte. :(\n';
+        eval_status.m2 = vampireOutput.m !== '' ? "Štruktúra " + vampireOutput.m + structureFormalizationToSolution
+            : "Štruktúra" + structureSolutionToFormalization ;
+        eval_status.iSolutionToFormalization = vampireOutput.prvky !== undefined ? vampireOutput.prvky : notFound;
         res.status(200).json(eval_status);
         saveSolutionWithResult(eval_status);
     }
@@ -65,9 +74,9 @@ module.exports = async function evalWithVampire(
           let structure = stdout.slice(stdout.indexOf("tff"), stdout.length);
           structure = structure.slice(0, structure.indexOf("% SZS"));
           structure = getStructure(structure, language, exercise);
-          return {status: setStatus(result), domain: structure.domain, prvky: structure.prvky};
+          return {status: setStatus(result), domain: structure.domain, prvky: structure.prvky, m: structure.m};
       }
-      return {status: setStatus(result), domain: "", predicates: ""};
+      return {status: setStatus(result), domain: "", predicates: "", m: ""};
   }
 
 const toVampireInput = (lhs, rhs) => `fof(a,axiom,${lhs}). fof(b,conjecture,${rhs}).`;
