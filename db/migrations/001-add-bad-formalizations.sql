b61b1d7e33193ade3b8cfc3f99b8106cbff26899
karin <karinka7117@gmail.com>
2023-06-01 18:00:22 +0200

    change migration scripts

--- db/migrations/001-add-bad-formalizations.sql
+++ db/migrations/001-add-bad-formalizations.sql
@@ -7,32 +7,6 @@ CREATE TABLE bad_formalizations(
     ON DELETE CASCADE
 );
 
-CREATE TABLE feedbacks(
-    feedback_id SERIAL PRIMARY KEY,
-    feedback TEXT,
-    bad_formalization_id INTEGER NOT NULL,
-    author TEXT,
-    active BOOLEAN,
-    FOREIGN KEY (bad_formalization_id)
-        REFERENCES bad_formalizations(bad_formalization_id)
-    ON DELETE CASCADE
-);
-
-CREATE TABLE feedback_to_solution(
-    id SERIAL PRIMARY KEY,
-    feedback_id INTEGER NOT NULL,
-    solution_id INTEGER NOT NULL,
-    rating INTEGER NOT NULL check (rating between -1 and 1),
-    showed timestamp NOT NULL,
-    rated timestamp NOT NULL,
-    FOREIGN KEY (feedback_id)
-        REFERENCES feedbacks(feedback_id)
-    ON DELETE CASCADE,
-    FOREIGN KEY (solution_id)
-        REFERENCES solutions(solution_id)
-    ON DELETE CASCADE
-);
-
 ALTER TABLE solutions
     ADD COLUMN formalization_id INTEGER DEFAULT null,
     ADD COLUMN bad_formalization_id INTEGER DEFAULT null,
--- db/migrations/002-feedback-to-solution.sql
+++ db/migrations/002-feedback-to-solution.sql
@@ -1,8 +1,26 @@
-ALTER TABLE feedback_to_solution
-    ALTER COLUMN rated SET DEFAULT NULL;
+CREATE TABLE feedbacks(
+    feedback_id SERIAL PRIMARY KEY,
+    feedback TEXT,
+    bad_formalization_id INTEGER NOT NULL,
+    author TEXT,
+    active BOOLEAN,
+    created timestamp,
+    FOREIGN KEY (bad_formalization_id)
+        REFERENCES bad_formalizations(bad_formalization_id)
+    ON DELETE CASCADE
+);
 
-ALTER TABLE feedback_to_solution
-    ALTER COLUMN rated DROP NOT NULL;
-
-ALTER TABLE feedbacks
-    ADD COLUMN created timestamp;
+CREATE TABLE feedback_to_solution(
+    id SERIAL PRIMARY KEY,
+    feedback_id INTEGER NOT NULL,
+    solution_id INTEGER NOT NULL,
+    rating INTEGER DEFAULT NULL check (rating between -1 and 1),
+    showed timestamp NOT NULL,
+    rated timestamp DEFAULT NULL,
+    FOREIGN KEY (feedback_id)
+        REFERENCES feedbacks(feedback_id)
+    ON DELETE CASCADE,
+    FOREIGN KEY (solution_id)
+        REFERENCES solutions(solution_id)
+    ON DELETE CASCADE
+);
